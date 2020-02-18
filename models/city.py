447784dc661d5dade10d1f73947b86b089b4acee
MAZTRO <design.mallet@gmail.com>
2020-02-18 14:59:31 +0000

    Add the first files

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+""" Class city """
+
+
+class City(BaseModel):
+    """ class City """
+    state_id = ""
+    name = ""
---
+++ models/user.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+""" Class User """
+
+
+class User(BaseModel):
+    """ class USER """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
